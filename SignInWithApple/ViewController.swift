cc27da69f79415ca3a6b5df5526a95960237b356
Ari Jain <thearijain@gmail.com>
2021-05-15 12:29:24 -0400

    Initial Commit

---
+++ SignInWithApple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SignInWithApple
+//
+//  Created by Ari Jain on 5/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
